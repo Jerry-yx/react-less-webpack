3b72810c2d7fba555ad91bb47bf4bdf273c6143b
Yuan Xuyan <yuanxuyan@yijiahe.com>
2018-03-16 16:49:20 +0800

    新增less loader css-loader style-loader postcss-loader
    Signed-off-by: Yuan Xuyan <yuanxuyan@yijiahe.com>

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+module.exports ={
+	entry: __dirname +'/app/index.js',//__dirname node.js全局变量 脚本所在目录
+	output: {
+		filename: 'bundle.js',
+		path: __dirname + "/public"
+	},
+	devtool: 'eval-source-map',//方便调试 cheap-module-eval-source-map大型项目中打包速度快不方便调试
+	devServer: {//安装npm install -g webpack-dev-server 搭建本地服务器
+		contentBase: './public',//服务器加载页面得目录
+		historyApiFallback: true,//不跳转
+		hot:true,
+		inline: true//实时刷新
+	},
+	module: {
+		rules: [
+			{
+				test:/(\.jsx|\.js)$/,//正则表达式删选文件
+				use:{
+					loader:'babel-loader',//loader
+					// options:{ 放于 .babelrc文件中
+					// 	presets:[//预处理loader
+					// 		"env","react"
+					// 	]
+					// }
+				},
+				exclude: /node_modules/ //排除不需要处理文件
+			},{
+				test:/\.css$/,
+				use: [
+					{
+						loader: "style-loader"//将所有的计算后的样式加入页面中
+					},{
+						loader: 'css-loader',//能够使用类似@import 和 url(...)的方法
+						options: {
+							modules: true,
+							localIdentName: '[name]_[local]--[hash:base64:5]'//制定css类名格式 可防止局部css污染全局样式
+						}
+					},{
+						loader:'postcss-loader'
+					}
+				]
+
+			},{
+				test: /\.less$/,
+				use:{
+					loader:'less-loader'
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
